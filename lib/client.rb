3327dc48191713043e8460a8212f39524adc5d91
Cory Ribson <crbi2550@gmail.com>
2015-05-08 10:23:41 -0700

    add finished spec and methods for Client class

--- lib/client.rb
+++ lib/client.rb
@@ -1,5 +1,40 @@
 class Client
-  attr_reader(:id, :client_name)
+  attr_reader :client_name
+  attr_accessor :id
 
+  def initialize(attributes)
+    @client_name = attributes[:client_name]
+    @id = attributes[:id]
+  end
+
+  def self.all
+    returned_clients = []
+    clients = DB.exec("SELECT * FROM clients;")
+    clients.each do |client|
+      client_name = client["client_name"]
+      id = client.fetch("id").to_i
+      returned_clients.push(Client.new(client_name: client_name, id: id))
+    end
+    returned_clients
+  end
+
+  def save
+    client = DB.exec("INSERT INTO clients (client_name) VALUES ('#{@client_name}') RETURNING id;")
+    @id = client.first.fetch("id").to_i
+  end
+
+  def ==(another_client)
+    self.client_name.==(another_client.client_name).&(self.id.==(another_client.id))
+  end
+
+  def delete
+    DB.exec("DELETE FROM clients WHERE id = #{self.id};")
+  end
+
+  def update(attributes)
+    @client_name = attributes[:client_name]
+    @id = self.id
+    DB.exec("UPDATE clients set client_name = '#{@client_name}' WHERE id = #{@id};")
+  end
 
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,4 +7,47 @@ describe Client do
       expect(test_client.client_name).to(eq("Babs"))
     end
   end
+
+  describe('.all') do
+    it('will be empty at first') do
+      expect(Client.all).to(eq([]))
+    end
+  end
+
+  describe ('#save') do
+    it('stores a client to the database') do
+      test_client = Client.new(client_name: "Babs Bunny")
+      test_client.save
+      expect(Client.all).to(eq([test_client]))
+    end
+  end
+
+  describe ('#==') do
+    it('is the same client if they have the same name') do
+      client1 = Client.new(client_name: "Elmira")
+      client2 = Client.new(client_name: "Elmira")
+      expect(client1).to(eq(client2))
+    end
+  end
+
+  describe('#delete') do
+    it('will delete a client from the salon') do
+      test_client = Client.new(client_name: "Tweety")
+      test_client.save
+      test_client2 = Client.new(client_name: "Babs")
+      test_client2.save
+      test_client.delete
+      expect(Client.all).to(eq([test_client2]))
+    end
+  end
+
+  describe('#update') do
+    it('lets the user update a client name in the DB') do
+      client = Client.new(client_name: "Elmer")
+      client.save()
+      client.update(client_name: "Elmer Fudd")
+      expect(client.client_name).to(eq("Elmer Fudd"))
+    end
+  end
+
 end
